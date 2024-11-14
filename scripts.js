2d888f1d6bbc3993ee3fcc9c5b7de51b95c0de3f
Cristian <dominicanxxx@gmail.com>
2024-11-14 16:45:51 -0400

    Added responsiveness for mobile devices
    Added  hamburger menu and media queries for mobile devices

--- scripts.js
+++ scripts.js
@@ -3,6 +3,7 @@ const projectsTiles = document.getElementsByClassName("project-tile");
 const skillsFill = document.getElementsByClassName("fill");
 const services = document.getElementsByClassName("service");
 const profileImg = document.getElementsByClassName("profile-img")[0];
+const hamburgerIcon = document.getElementsByClassName("icon")[0];
 
 const options = {
   threshold: 0.5,
@@ -43,4 +44,31 @@ service.forEach((service) => {
 const skills = Array.from(skillsFill);
 skills.forEach((skill) => {
   observer.observe(skill);
+});
+
+function displayMobileMenu() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
+function viewportChanged(x) {
+  if (x.matches) {
+    // If media query matches
+    const myLinks = document.getElementById("myLinks");
+    myLinks.style.display = "block";
+  }
+}
+
+// Create a MediaQueryList object
+var x = window.matchMedia("(min-width: 767px)");
+
+// Call listener function at run time
+viewportChanged(x);
+
+// Attach listener function on state changes
+x.addEventListener("change", function () {
+  viewportChanged(x);
 });
